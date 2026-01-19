6c0ecb7bb088d94168bd065e3d8737064c907d3d
Steve Trout <steve@trout.rocks>
2026-01-19 00:49:10 -0600

    Archive Ral league and create ECL stub

---
+++ leagues/ecl/ecl.ts
@@ -0,0 +1,14 @@
+import { Client, Interaction, Message } from "discord.js";
+import { Handler } from "../../dispatch.ts";
+
+export function setup(): Promise<{
+    watch: (client: Client) => Promise<void>;
+    messageHandlers: Handler<Message>[];
+    interactionHandlers: Handler<Interaction>[];
+}> {
+    return Promise.resolve({
+        watch: () => Promise.resolve(),
+        messageHandlers: [],
+        interactionHandlers: [],
+    });
+}
